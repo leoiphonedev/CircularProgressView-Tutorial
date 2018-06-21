0d83166818b0e685def4a089102842ea4012fc68
leoiphonedev <aggarwalaman1985@gmail.com>
2018-06-21 21:07:18 +0530

    Final source code

---
+++ CircularProgressView-Tutorial/CircularProgressView.swift
@@ -0,0 +1,77 @@
+//
+//  CircularProgressView.swift
+//  CircularProgressView-Tutorial
+//
+//  Created by Aman Aggarwal on 20/05/18.
+//  Copyright © 2018 Aman Aggarwal. All rights reserved.
+//
+
+import UIKit
+
+class CircularProgressView: UIView {
+
+    fileprivate var progressLayer = CAShapeLayer()
+    fileprivate var trackLayer = CAShapeLayer()
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        createCircularPath()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        createCircularPath()
+    }
+    
+    var progressColor = UIColor.white {
+        didSet {
+            progressLayer.strokeColor = progressColor.cgColor
+        }
+    }
+    
+    var trackColor = UIColor.white {
+        didSet {
+            trackLayer.strokeColor = trackColor.cgColor
+        }
+    }
+    
+    fileprivate func createCircularPath() {
+        self.backgroundColor = UIColor.clear
+        self.layer.cornerRadius = self.frame.size.width/2
+        let circlePath = UIBezierPath(arcCenter: CGPoint(x: frame.size.width/2, y: frame.size.height/2), radius: (frame.size.width - 1.5)/2, startAngle: CGFloat(-0.5 * .pi), endAngle: CGFloat(1.5 * .pi), clockwise: true)
+        trackLayer.path = circlePath.cgPath
+        trackLayer.fillColor = UIColor.clear.cgColor
+        trackLayer.strokeColor = trackColor.cgColor
+        trackLayer.lineWidth = 10.0
+        trackLayer.strokeEnd = 1.0
+        layer.addSublayer(trackLayer)
+        
+        progressLayer.path = circlePath.cgPath
+        progressLayer.fillColor = UIColor.clear.cgColor
+        progressLayer.strokeColor = progressColor.cgColor
+        progressLayer.lineWidth = 10.0
+        progressLayer.strokeEnd = 0.0
+        layer.addSublayer(progressLayer)
+    }
+    
+    func setProgressWithAnimation(duration: TimeInterval, value: Float) {
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        animation.duration = duration
+        animation.fromValue = 0
+        animation.toValue = value
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        progressLayer.strokeEnd = CGFloat(value)
+        progressLayer.add(animation, forKey: "animateprogress")
+    }
+    
+    
+
+}
--- CircularProgressView-Tutorial/ViewController.swift
+++ CircularProgressView-Tutorial/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var CircularProgress: CircularProgressView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let cp = CircularProgressView(frame: CGRect(x: 10.0, y: 10.0, width: 100.0, height: 100.0))
+        cp.trackColor = UIColor.white
+        cp.progressColor = UIColor(red: 252.0/255.0, green: 141.0/255.0, blue: 165.0/255.0, alpha: 1.0)
+        cp.tag = 101
+        self.view.addSubview(cp)
+        cp.center = self.view.center
+        
+        self.perform(#selector(animateProgress), with: nil, afterDelay: 2.0)
+        
+        CircularProgress.trackColor = UIColor.white
+        CircularProgress.progressColor = UIColor.purple
+        CircularProgress.setProgressWithAnimation(duration: 1.0, value: 0.3)
     }
 
+    @objc func animateProgress() {
+        let cP = self.view.viewWithTag(101) as! CircularProgressView
+        cP.setProgressWithAnimation(duration: 1.0, value: 0.7)
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
